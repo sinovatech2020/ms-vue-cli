47268a7be278f6649f28b5a9ac61f82bd3e6b622
liuxiyuan <liuxiyuan@sinovatech.com>
2020-11-11 17:07:24 +0800

    增加移动端适配及css初始化文件

--- src/main.js
+++ src/main.js
@@ -4,19 +4,15 @@ import router from "./router";
 import store from "./store";
 
 // 引入公共css
-import './libs/css/rest.css';
+import "./libs/css/rest.css";
 // 引入rem
-import rem from './libs/js/public';
-rem(window,1242);
-
-// 引入联通
-import UnicomJSBrige from "./libs/js/unicom-jsbrige-vue";
-Vue.use(UnicomJSBrige);
+// import rem from './libs/js/public';
+// rem(window,1242);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: (h) => h(App),
 }).$mount("#app");
