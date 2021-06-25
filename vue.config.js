2cafb33bdc401af53057331122c80d838737f24e
liuxiyuan <liuxiyuan@sinovatech.com>
2021-06-25 10:48:14 +0800

    px转rem

--- vue.config.js
+++ vue.config.js
@@ -84,6 +84,22 @@ module.exports = {
 			},
 		},
 	},
+    css: {
+        loaderOptions: {
+            postcss: {
+                plugins: [
+                    require("postcss-pxtorem")({
+                        rootValue: 100,
+                        propWhiteList: [],
+                        // 注意：如果有使用第三方UI如Vant，则需要配置下忽略选择器不转换。
+                        // 规则是class中包含的字符串，如Vant中所有的class前缀都是van-。也可以是正则。
+                        "selectorBlackList": ["van-"]
+                    }),
+                    require("autoprefixer")()
+                ]
+            }
+        }
+    },
 	chainWebpack: config => {
 		config.plugins.delete("prefetch");
 		const jsRule = config.module.rule("js");
